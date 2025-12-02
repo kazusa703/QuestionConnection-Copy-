a8472816226c705ce5ccd97ee0127c3f0228330a
imai kazusa <kazusa1304@icloud.com>
2025-12-02 21:34:58 +0900

    ああ

---
+++ QuestionConnection/Views/PremiumPlanView.swift
@@ -0,0 +1,18 @@
+//
+//  PremiumPlanView.swift
+//  QuestionConnection
+//
+//  Created by 今井一颯 on 2025/12/02.
+//
+
+import SwiftUI
+
+struct PremiumPlanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PremiumPlanView()
+}
