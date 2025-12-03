ad1b562c24ec38a4c80f5ee7f7666cbe44b3672f
imai kazusa <kazusa1304@icloud.com>
2025-12-03 14:56:46 +0900

    おい

---
+++ QuestionConnection/Views/ModelAnswerView.swift
@@ -0,0 +1,18 @@
+//
+//  ModelAnswerView.swift
+//  QuestionConnection
+//
+//  Created by 今井一颯 on 2025/12/03.
+//
+
+import SwiftUI
+
+struct ModelAnswerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelAnswerView()
+}
