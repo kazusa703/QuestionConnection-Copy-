457727f8508b1b743aa00ec8d0a1f6e4eec95b37
imai kazusa <kazusa1304@icloud.com>
2025-12-07 17:50:03 +0900

    あ

---
+++ QuestionConnection/Views/AnswerResultView.swift
@@ -0,0 +1,18 @@
+//
+//  AnswerResultView.swift
+//  QuestionConnection
+//
+//  Created by 今井一颯 on 2025/12/07.
+//
+
+import SwiftUI
+
+struct AnswerResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnswerResultView()
+}
