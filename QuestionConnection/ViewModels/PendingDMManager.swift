2b04b2cb0606259e61dab750aa8ff03e9d5186e7
imai kazusa <kazusa1304@icloud.com>
2025-12-14 02:18:05 +0900

    a

---
+++ QuestionConnection/ViewModels/PendingDMManager.swift
@@ -0,0 +1,8 @@
+//
+//  PendingDMManager.swift
+//  QuestionConnection
+//
+//  Created by 今井一颯 on 2025/12/14.
+//
+
+import Foundation
---
+++ QuestionConnection/Views/DMGuideView.swift
@@ -0,0 +1,18 @@
+//
+//  DMGuideView.swift
+//  QuestionConnection
+//
+//  Created by 今井一颯 on 2025/12/14.
+//
+
+import SwiftUI
+
+struct DMGuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DMGuideView()
+}
