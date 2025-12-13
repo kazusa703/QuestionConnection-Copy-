099c68ff3619d7499da9d1d06f2854598778408a
imai kazusa <kazusa1304@icloud.com>
2025-12-13 16:42:22 +0900

    あ

---
+++ QuestionConnection/Views/FillInBoxView.swift
@@ -0,0 +1,18 @@
+//
+//  FillInBoxView.swift
+//  QuestionConnection
+//
+//  Created by 今井一颯 on 2025/12/13.
+//
+
+import SwiftUI
+
+struct FillInBoxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FillInBoxView()
+}
