1d4ad57e1f5ad831b4148c492cf1a24705b08aee
imai kazusa <kazusa1304@icloud.com>
2025-12-14 16:40:28 +0900

    ああ

---
+++ QuestionConnection/Views/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  QuestionConnection
+//
+//  Created by 今井一颯 on 2025/12/14.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
