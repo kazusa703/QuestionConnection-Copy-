986944ef40e0ddfb809d16eaa60e9fab8c8ff4b3
imai kazusa <kazusa1304@icloud.com>
2025-12-15 11:39:20 +0900

    a

---
+++ QuestionConnection/Views/HelpHintButton.swift
@@ -0,0 +1,18 @@
+//
+//  HelpHintButton.swift
+//  QuestionConnection
+//
+//  Created by 今井一颯 on 2025/12/15.
+//
+
+import SwiftUI
+
+struct HelpHintButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HelpHintButton()
+}
