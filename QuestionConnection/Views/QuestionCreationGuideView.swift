c1db6cd2d2cfbfee25c8debe0d439239d2a26eef
imai kazusa <kazusa1304@icloud.com>
2025-12-18 13:39:56 +0900

    あ

---
+++ QuestionConnection/Views/QuestionCreationGuideView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionCreationGuideView.swift
+//  QuestionConnection
+//
+//  Created by 今井一颯 on 2025/12/18.
+//
+
+import SwiftUI
+
+struct QuestionCreationGuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionCreationGuideView()
+}
